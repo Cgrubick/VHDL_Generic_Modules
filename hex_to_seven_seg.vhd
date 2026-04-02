89268e074c8c21e0bd0d3fb1bd68a0ff1b381b10
cgrubick <cgrubick@gmail.com>
2026-04-01 20:51:22 -0400

    hex 2 7 segment translated

--- hex_to_seven_seg.vhd
+++ hex_to_seven_seg.vhd
@@ -1,26 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
-module hex_to_seven_seg(
-    input [3:0] hex,
-    output reg[6:0] seven_seg
+entity hex_to_seven_seg is
+    port (
+        hex       : in  std_logic_vector(3 downto 0);
+        seven_seg : out std_logic_vector(6 downto 0)
     );
-    
-    always @(hex) 
-        case(hex) //gfedcba
-            0: seven_seg = 7'b1000000;    
-            1: seven_seg = 7'b1111001;  
-            2: seven_seg = 7'b0100100;  
-            3: seven_seg = 7'b0110000;  
-            4: seven_seg = 7'b0011001;  
-            5: seven_seg = 7'b0010010;  
-            6: seven_seg = 7'b0000010;  
-            7: seven_seg = 7'b1111000;  
-            8: seven_seg = 7'b0000000;  
-            9: seven_seg = 7'b0010000;  
-            10: seven_seg = 7'b0001000;  
-            11: seven_seg = 7'b1000011;  
-            12: seven_seg = 7'b1000110;  
-            13: seven_seg = 7'b0100001;  
-            14: seven_seg = 7'b0000110;  
-            15: seven_seg = 7'b0001110;  
-        endcase           
-endmodule
+end entity hex_to_seven_seg;
+
+architecture rtl of hex_to_seven_seg is
+begin
+
+    process(hex)
+    begin
+        case hex is  -- gfedcba
+            when x"0" => seven_seg <= "1000000";
+            when x"1" => seven_seg <= "1111001";
+            when x"2" => seven_seg <= "0100100";
+            when x"3" => seven_seg <= "0110000";
+            when x"4" => seven_seg <= "0011001";
+            when x"5" => seven_seg <= "0010010";
+            when x"6" => seven_seg <= "0000010";
+            when x"7" => seven_seg <= "1111000";
+            when x"8" => seven_seg <= "0000000";
+            when x"9" => seven_seg <= "0010000";
+            when x"A" => seven_seg <= "0001000";
+            when x"B" => seven_seg <= "1000011";
+            when x"C" => seven_seg <= "1000110";
+            when x"D" => seven_seg <= "0100001";
+            when x"E" => seven_seg <= "0000110";
+            when x"F" => seven_seg <= "0001110";
+            when others => seven_seg <= "1111111";
+        end case;
+    end process;
+
+end architecture rtl;
