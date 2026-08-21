99ebcb085cafda26a22a8908f0b179247cc3ae9c
cgrubick <cgrubick@gmail.com>
2026-08-21 16:07:58 -0400

    cocotb tb for async fifo

--- async_fifo/async_fifo.vhd
+++ async_fifo/async_fifo.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
+use ieee.math_real.all; 
 
 entity async_fifo is
     generic (
@@ -30,34 +30,39 @@ end entity async_fifo;
 
 architecture rtl of async_fifo is
 
+    constant ADDR_W  : integer := integer(ceil(log2(real(DEPTH))));
+    
     type async_fifo_t is array (0 to DEPTH - 1) of std_logic_vector(WIDTH - 1 downto 0);
     signal fifo : async_fifo_t;
 
-    signal wr_ptr_gray      : unsigned(WIDTH - 1 downto 0);
-    signal rd_ptr_gray      : unsigned(WIDTH - 1 downto 0);
+    signal wr_ptr_gray      : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_gray      : unsigned(ADDR_W - 1 downto 0);
 
     
-    signal wr_ptr           : unsigned(WIDTH - 1 downto 0);
-    signal rd_ptr           : unsigned(WIDTH - 1 downto 0);
-    signal wr_ptr_rd_clk_q  : unsigned(WIDTH - 1 downto 0);
-    signal rd_ptr_wr_clk_q  : unsigned(WIDTH - 1 downto 0);
-    signal wr_ptr_rd_clk    : unsigned(WIDTH - 1 downto 0);
-    signal rd_ptr_wr_clk    : unsigned(WIDTH - 1 downto 0);
+    signal wr_ptr           : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr           : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr_rd_clk_q  : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_wr_clk_q  : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr_rd_clk    : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_wr_clk    : unsigned(ADDR_W - 1 downto 0);
     
 begin
 
-    full    <= '1' when rd_ptr_wr_clk = wr_ptr else '0';
+    
+    full    <= '1' when not(rd_ptr_wr_clk) = wr_ptr else '0';
     empty   <= '1' when wr_ptr_rd_clk = rd_ptr else '0';
-    ae      <= '1';
-    af      <= '1';
+    af      <= '1' when not(rd_ptr_wr_clk) = wr_ptr - AF_LEVEL else '0';
+    ae      <= '1' when wr_ptr_rd_clk = rd_ptr + 1 else '0';
     -- Write pointer on write clock domain
     process (wr_clk, wr_rst_n)
     begin
         if wr_rst_n = '0' then
-            
+            wr_ptr      <= (others => '0');
+            wr_ptr_gray <= (others => '0');
         elsif rising_edge(wr_clk) then
-            if(wr_en and not full) then
+            if(wr_en = '1' and full  = '0') then
                 wr_ptr <= wr_ptr + 1;
+                wr_ptr_gray <= shift_right(wr_ptr + 1, 1) xor wr_ptr + 1;
             else
                 wr_ptr <= wr_ptr;
             end if;
@@ -68,10 +73,12 @@ begin
     process (rd_clk, rd_rst_n)
     begin
         if rd_rst_n = '0' then
-            
+            rd_ptr      <= (others => '0');
+            rd_ptr_gray <= (others => '0');
         elsif rising_edge(rd_clk) then
-            if(rd_en and not empty) then
+            if(rd_en = '1' and empty = '0') then
                 rd_ptr <= rd_ptr + 1;
+                rd_ptr_gray <= shift_right(rd_ptr + 1, 1) xor rd_ptr + 1;
             else
                 rd_ptr <= rd_ptr;
             end if;
@@ -85,7 +92,7 @@ begin
             wr_ptr_rd_clk_q <= (others => '0');
             wr_ptr_rd_clk   <= (others => '0');
         elsif rising_edge(rd_clk) then
-            wr_ptr_rd_clk_q <= wr_ptr;
+            wr_ptr_rd_clk_q <= wr_ptr_gray;
             wr_ptr_rd_clk   <= wr_ptr_rd_clk_q;
         end if;
     end process;
@@ -97,7 +104,7 @@ begin
             rd_ptr_wr_clk_q <= (others => '0');
             rd_ptr_wr_clk   <= (others => '0');
         elsif rising_edge(wr_clk) then
-            rd_ptr_wr_clk_q <= rd_ptr;
+            rd_ptr_wr_clk_q <= rd_ptr_gray;
             rd_ptr_wr_clk   <= rd_ptr_wr_clk_q;
         end if;
     end process;
@@ -108,13 +115,13 @@ begin
         if wr_rst_n = '0' then
 
         elsif rising_edge(wr_clk) then
-            if(wr_en and not full) then 
+            if(wr_en = '1' and full = '0') then 
                 fifo(to_integer(wr_ptr)) <= wr_data;
             end if;
         end if;
     end process;
 
     -- reading out data from fifo
-    rd_data <= fifo(to_integer(rd_ptr)) when rd_en and not empty else (others => '0');
+    rd_data <= fifo(to_integer(rd_ptr)) when rd_en = '1' and empty = '0' else (others => '0');
 
 end architecture;
---
+++ async_fifo/sim/async_fifo_tb.py
@@ -0,0 +1,21 @@
+import cocotb
+from cocotb.triggers import FallingEdge, Timer
+from cocotb.clock import Clock
+
+@cocotb.test()
+async def async_fifo_tb(dut):
+
+    cocotb.start_soon(Clock(dut.wr_clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.rd_clk, 5, units="ns").start())
+    await Timer(50, unit="ns")
+    cocotb.log.info("full is %s", dut.full.value)
+    cocotb.log.info("empty is %s", dut.empty.value)
+    cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
+    await Timer(50, unit="ns")
+    cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
+    await Timer(5, unit="ns")
+    assert dut.wr_clk.value == 1
+    await Timer(5, unit="ns")
+    assert dut.rd_clk.value == 1
+    await Timer(5, unit="ns")
+    assert dut.rd_clk.value == 1
