b0b9c1348499e0dde68eb767abb617f49d7677fa
Clayton Grubick <ccg0015@mix.wvu.edu>
2026-08-21 17:03:27 -0400

    updateed timer calls in cocotb

--- async_fifo/sim/async_fifo_tb.py
+++ async_fifo/sim/async_fifo_tb.py
@@ -7,15 +7,15 @@ async def async_fifo_tb(dut):
 
     cocotb.start_soon(Clock(dut.wr_clk, 10, units="ns").start())
     cocotb.start_soon(Clock(dut.rd_clk, 5, units="ns").start())
-    await Timer(50, unit="ns")
+    await Timer(50, units="ns")
     cocotb.log.info("full is %s", dut.full.value)
     cocotb.log.info("empty is %s", dut.empty.value)
     cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
-    await Timer(50, unit="ns")
+    await Timer(50, units="ns")
     cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
-    await Timer(5, unit="ns")
+    await Timer(5, units="ns")
     assert dut.wr_clk.value == 1
-    await Timer(5, unit="ns")
+    await Timer(5, units="ns")
     assert dut.rd_clk.value == 1
-    await Timer(5, unit="ns")
+    await Timer(5, units="ns")
     assert dut.rd_clk.value == 1
