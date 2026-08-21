7ca1f128269e02318e791f3ec7d1b31994bd27a9
Clayton Grubick <ccg0015@mix.wvu.edu>
2026-08-21 17:27:06 -0400

    reverted syntax for cocotb 2.0.1

--- async_fifo/sim/async_fifo_tb.py
+++ async_fifo/sim/async_fifo_tb.py
@@ -5,17 +5,17 @@ from cocotb.clock import Clock
 @cocotb.test()
 async def async_fifo_tb(dut):
 
-    cocotb.start_soon(Clock(dut.wr_clk, 10, units="ns").start())
-    cocotb.start_soon(Clock(dut.rd_clk, 5, units="ns").start())
-    await Timer(50, units="ns")
+    cocotb.start_soon(Clock(dut.wr_clk, 10, unit="ns").start())
+    cocotb.start_soon(Clock(dut.rd_clk, 5, unit="ns").start())
+    await Timer(50, unit="ns")
     cocotb.log.info("full is %s", dut.full.value)
     cocotb.log.info("empty is %s", dut.empty.value)
     cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
-    await Timer(50, units="ns")
+    await Timer(50, unit="ns")
     cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
-    await Timer(5, units="ns")
+    await Timer(5, unit="ns")
     assert dut.wr_clk.value == 1
-    await Timer(5, units="ns")
+    await Timer(5, unit="ns")
     assert dut.rd_clk.value == 1
-    await Timer(5, units="ns")
+    await Timer(5, unit="ns")
     assert dut.rd_clk.value == 1
