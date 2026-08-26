c9152d3fe83a3ef81aa0a493315396bb28aceb77
cgrubick <cgrubick@gmail.com>
2026-08-26 06:44:35 -0400

    adjusted empty and full ptrs

--- async_fifo/async_fifo.vhd
+++ async_fifo/async_fifo.vhd
@@ -35,25 +35,26 @@ architecture rtl of async_fifo is
     type async_fifo_t is array (0 to DEPTH - 1) of std_logic_vector(WIDTH - 1 downto 0);
     signal fifo : async_fifo_t;
 
-    signal wr_ptr_gray      : unsigned(ADDR_W - 1 downto 0);
-    signal rd_ptr_gray      : unsigned(ADDR_W - 1 downto 0);
-
-    
-    signal wr_ptr           : unsigned(ADDR_W - 1 downto 0);
-    signal rd_ptr           : unsigned(ADDR_W - 1 downto 0);
-    signal wr_ptr_rd_clk_q  : unsigned(ADDR_W - 1 downto 0);
-    signal rd_ptr_wr_clk_q  : unsigned(ADDR_W - 1 downto 0);
-    signal wr_ptr_rd_clk    : unsigned(ADDR_W - 1 downto 0);
-    signal rd_ptr_wr_clk    : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr_gray          : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_gray          : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr_gray_next     : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_gray_next     : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr               : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr               : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr_rd_clk_q      : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_wr_clk_q      : unsigned(ADDR_W - 1 downto 0);
+    signal wr_ptr_rd_clk        : unsigned(ADDR_W - 1 downto 0);
+    signal rd_ptr_wr_clk        : unsigned(ADDR_W - 1 downto 0);
     
 begin
 
     
-    full    <= '1' when not(rd_ptr_wr_clk) = wr_ptr else '0';
-    empty   <= '1' when wr_ptr_rd_clk = rd_ptr else '0';
-    af      <= '1' when not(rd_ptr_wr_clk) = wr_ptr - AF_LEVEL else '0';
-    ae      <= '1' when wr_ptr_rd_clk = rd_ptr + 1 else '0';
+    full    <= '1' when not(rd_ptr_wr_clk) = wr_ptr_gray_next else '0';
+    empty   <= '1' when wr_ptr_rd_clk = rd_ptr_gray_next else '0';
+    af      <= '1' when not(rd_ptr_wr_clk) = wr_ptr_gray_next - AF_LEVEL else '0';
+    ae      <= '1' when wr_ptr_rd_clk = rd_ptr_gray_next + 1 else '0';
     -- Write pointer on write clock domain
+    wr_ptr_gray_next <= shift_right(wr_ptr + 1, 1) xor wr_ptr + 1;
     process (wr_clk, wr_rst_n)
     begin
         if wr_rst_n = '0' then
@@ -61,8 +62,8 @@ begin
             wr_ptr_gray <= (others => '0');
         elsif rising_edge(wr_clk) then
             if(wr_en = '1' and full  = '0') then
-                wr_ptr <= wr_ptr + 1;
-                wr_ptr_gray <= shift_right(wr_ptr + 1, 1) xor wr_ptr + 1;
+                wr_ptr      <= wr_ptr + 1;
+                wr_ptr_gray <= wr_ptr_gray_next;
             else
                 wr_ptr <= wr_ptr;
             end if;
@@ -70,6 +71,7 @@ begin
     end process;
 
     -- Read pointer on read clock domain
+    rd_ptr_gray_next <= shift_right(rd_ptr + 1, 1) xor rd_ptr + 1;
     process (rd_clk, rd_rst_n)
     begin
         if rd_rst_n = '0' then
@@ -77,8 +79,8 @@ begin
             rd_ptr_gray <= (others => '0');
         elsif rising_edge(rd_clk) then
             if(rd_en = '1' and empty = '0') then
-                rd_ptr <= rd_ptr + 1;
-                rd_ptr_gray <= shift_right(rd_ptr + 1, 1) xor rd_ptr + 1;
+                rd_ptr      <= rd_ptr + 1;
+                rd_ptr_gray <= rd_ptr_gray_next;
             else
                 rd_ptr <= rd_ptr;
             end if;
@@ -122,6 +124,6 @@ begin
     end process;
 
     -- reading out data from fifo
-    rd_data <= fifo(to_integer(rd_ptr)) when rd_en = '1' and empty = '0' else (others => '0');
+    rd_data <= fifo(to_integer(rd_ptr)) when rd_en = '1' and empty = '0' else (others => '1');
 
 end architecture;
--- async_fifo/sim/async_fifo_tb.py
+++ async_fifo/sim/async_fifo_tb.py
@@ -1,21 +1,30 @@
 import cocotb
-from cocotb.triggers import FallingEdge, Timer
 from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Edge, Timer, ReadOnly, First
 
+async def full_reset(dut):
+    dut.wr_en.value = 0
+    dut.rd_en.value = 0
+    dut.wr_rst_n.value = 0
+    dut.rd_rst_n.value = 0
+    await Timer(50, unit="ns")
+    dut.wr_rst_n.value = 1
+    dut.rd_rst_n.value = 1
+    await Timer(50, unit="ns")
+    dut._log.info("RSTs: wr=%s, rd=%s", dut.wr_rst_n.value, dut.rd_rst_n.value)
+
+async def empty_check(dut):
+    assert dut.empty.value == 1, f"empty = {dut.empty.value}"
+
+async def ptr_dump(dut):
+   dut._log.info("ptrs: wr=%s wr_gray=%s rd=%s rd_gray=%s", dut.wr_ptr.value, dut.wr_ptr_gray.value, dut.rd_ptr.value, dut.rd_ptr_gray.value)
 @cocotb.test()
 async def async_fifo_tb(dut):
-
+    
     cocotb.start_soon(Clock(dut.wr_clk, 10, unit="ns").start())
     cocotb.start_soon(Clock(dut.rd_clk, 5, unit="ns").start())
+    await full_reset(dut)
     await Timer(50, unit="ns")
-    cocotb.log.info("full is %s", dut.full.value)
-    cocotb.log.info("empty is %s", dut.empty.value)
-    cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
+    await ptr_dump(dut)
     await Timer(50, unit="ns")
-    cocotb.log.info("wr_clk is %s", dut.wr_clk.value)
-    await Timer(5, unit="ns")
-    assert dut.wr_clk.value == 1
-    await Timer(5, unit="ns")
-    assert dut.rd_clk.value == 1
-    await Timer(5, unit="ns")
-    assert dut.rd_clk.value == 1
+    await empty_check(dut)
